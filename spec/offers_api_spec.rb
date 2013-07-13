72abc3d294bd3c8b0213461baedcbf1b330ca6d9
Bruno Grasselli <bruno.grasselli@gmail.com>
2013-07-13 00:24:15 -0300

    creating the functional test for authentication

--- spec/offers_api_spec.rb
+++ spec/offers_api_spec.rb
@@ -4,9 +4,29 @@ describe OffersApi do
   let(:app) { described_class }
 
   describe "offers with GET" do
-    it "returns status code 200" do
-      get '/offers'
-      last_response.status.should eq 200
+    let(:user_api_key) { 'e95a21621a1865bcbae3bee89c4d4f84' }
+
+    context "when authentication hash is invalid" do
+      let(:invalid_hash_key) { 'invalid_hash' }
+
+      it "returns status code 401" do
+        get '/offers', hash_key: invalid_hash_key
+        last_response.status.should eq 401
+      end
+
+      it "returns body ERROR_INVALID_HASHKEY" do
+        get '/offers', hash_key: invalid_hash_key
+        last_response.body.should eq 'ERROR_INVALID_HASHKEY'
+      end
+    end
+
+    context "when authentication hash is valid" do
+      let(:valid_hash_key) { '7a2b1604c03d46eec1ecd4a686787b75dd693c4d' }
+
+      it "returns status code 200" do
+        get '/offers', hash_key: valid_hash_key
+        last_response.status.should eq 200
+      end
     end
   end
 end
