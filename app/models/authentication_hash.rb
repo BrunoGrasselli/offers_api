9c3b6eec130535ef64f8596fd1f2397ba6c55c62
Bruno Grasselli <bruno.grasselli@gmail.com>
2013-07-12 23:22:48 -0300

    implementing AuthenticationHash#request_hash

--- app/models/authentication_hash.rb
+++ app/models/authentication_hash.rb
@@ -1,5 +1,11 @@
 class AuthenticationHash
   def request_hash(parameters, api_key)
-    
+    Digest::SHA1.hexdigest "#{parameters_text(parameters)}&#{api_key}"
+  end
+
+  private
+
+  def parameters_text(parameters)
+    parameters.map{|k,v| "#{k}=#{v}"}.sort * '&'
   end
 end
--- spec/models/authentication_hash_spec.rb
+++ spec/models/authentication_hash_spec.rb
@@ -16,9 +16,14 @@ describe AuthenticationHash do
       timestamp: '1312553361'
     } }
 
-    it "generates response hash key" do
+    it "generates request hash for key 'e95a21621a1865bcbae3bee89c4d4f84'" do
       authentication_hash.request_hash(parameters, 'e95a21621a1865bcbae3bee89c4d4f84')
                          .should eq '7a2b1604c03d46eec1ecd4a686787b75dd693c4d'
+    end
+
+    it "generates request hash for key '99999999999999999999999999999999'" do
+      authentication_hash.request_hash(parameters, '99999999999999999999999999999999')
+                         .should eq '823fee17e1d84aa7c7030b4489a72ab73df84d9e'
     end
   end
 end
