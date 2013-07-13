93f88c6b69d2f89693c27985f66d40758ad5dae8
Bruno Grasselli <bruno.grasselli@gmail.com>
2013-07-13 10:33:45 -0300

    refactoring

--- app/offers_api.rb
+++ app/offers_api.rb
@@ -1,18 +1,24 @@
 class OffersApi < Sinatra::Base
   get "/offers" do
     application = Application.where(external_id: params[:appid]).first
-    authentication_hash = AuthenticationHash.new(application.api_key)
 
+    safe_halt(application) do |body|
+      body = ''
+    end
+  end
+
+  private
+
+  def safe_halt(application, &block)
+    authentication_hash = AuthenticationHash.new(application.api_key)
     verify_request_hash! authentication_hash, params[:hash_key]
 
-    body = ''
+    block.call(body = '')
 
     set_request_hash! authentication_hash, body
 
     halt 200, body
   end
-
-  private
 
   def verify_request_hash!(authentication_hash, hash_key)
     halt 401, 'ERROR_INVALID_HASHKEY' unless authentication_hash.valid_request?(params, hash_key)
