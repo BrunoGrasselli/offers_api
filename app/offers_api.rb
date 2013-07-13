96c5f4ea874c443225a08ce4a22f6cd72fa21a87
Bruno Grasselli <bruno.grasselli@gmail.com>
2013-07-13 09:59:12 -0300

    verifying request hash key

--- app/offers_api.rb
+++ app/offers_api.rb
@@ -1,5 +1,16 @@
 class OffersApi < Sinatra::Base
   get "/offers" do
+    application = Application.where(external_id: params[:appid]).first
+
+    verify_auth_hash! application, params[:hash_key]
+
     status 200
+  end
+
+  private
+
+  def verify_auth_hash!(application, hash_key)
+    authentication_hash = AuthenticationHash.new(application.api_key)
+    halt 401, 'ERROR_INVALID_HASHKEY' unless authentication_hash.valid_request_hash?(params, hash_key)
   end
 end
--- spec/offers_api_spec.rb
+++ spec/offers_api_spec.rb
@@ -4,18 +4,16 @@ describe OffersApi do
   let(:app) { described_class }
 
   describe "offers with GET" do
-    let(:user_api_key) { 'e95a21621a1865bcbae3bee89c4d4f84' }
+    let!(:application) { Application.create! api_key: 'e95a21621a1865bcbae3bee89c4d4f84', external_id: '123' }
 
     context "when authentication hash is invalid" do
-      let(:invalid_hash_key) { 'invalid_hash' }
-
       it "returns status code 401" do
-        get '/offers', hash_key: invalid_hash_key
+        get '/offers', hash_key: 'invalid_hash_key', appid: '123'
         last_response.status.should eq 401
       end
 
       it "returns body ERROR_INVALID_HASHKEY" do
-        get '/offers', hash_key: invalid_hash_key
+        get '/offers', hash_key: 'invalid_hash_key', appid: '123'
         last_response.body.should eq 'ERROR_INVALID_HASHKEY'
       end
     end
@@ -23,8 +21,14 @@ describe OffersApi do
     context "when authentication hash is valid" do
       let(:valid_hash_key) { '7a2b1604c03d46eec1ecd4a686787b75dd693c4d' }
 
+      before do
+        authentication_hash = AuthenticationHash.new(application.api_key)
+        authentication_hash.stub(:valid_request_hash?).with(anything, valid_hash_key).and_return(true)
+        AuthenticationHash.stub(:new).with(application.api_key).and_return(authentication_hash)
+      end
+
       it "returns status code 200" do
-        get '/offers', hash_key: valid_hash_key
+        get '/offers', hash_key: valid_hash_key, appid: '123'
         last_response.status.should eq 200
       end
     end
