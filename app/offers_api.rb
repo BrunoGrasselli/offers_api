900e5ad293e89c10cf26c91a177be8210c3e9a78
Bruno Grasselli <bruno.grasselli@gmail.com>
2013-07-14 12:18:27 -0300

    refactoring

--- app/offers_api.rb
+++ app/offers_api.rb
@@ -1,23 +1,28 @@
 class OffersApi < Sinatra::Base
-  get "/offers.:format" do
-    application = Application.where(external_id: params[:appid]).first
-    halt 400, 'ERROR_INVALID_APPID' unless application
+  KNOWN_PARAMS = ['appid', 'uid', 'pub0', 'page', 'device_id', 'locale', 'ip', 'offer_types']
 
-    safe_halt(application, 200) do
+  get "/offers.:format" do
+    application = load_application
+
+    safe_response(application, 200) do
       offers = Offer.by_types(params[:offer_types]).paginate(per_page: per_page, page: page)
 
       if offers.any?
         presenter = OffersPresenter.new(offers)
         presenter.send("to_#{params[:format]}")
-      else
-        ''
       end
     end
   end
 
   private
 
-  def safe_halt(application, code)
+  def load_application
+    application = Application.where(external_id: params[:appid]).first
+    halt 400, 'ERROR_INVALID_APPID' unless application
+    application
+  end
+
+  def safe_response(application, code)
     auth_hash = OffersSDK::AuthenticationHash.new(application.api_key)
     verify_request_hash! auth_hash
 
@@ -29,7 +34,7 @@ class OffersApi < Sinatra::Base
   end
 
   def verify_request_hash!(auth_hash)
-    filtered_params = params.reject {|k,v| !['appid', 'uid', 'pub0', 'page', 'device_id', 'locale', 'ip', 'offer_types'].include? k}
+    filtered_params = params.reject {|k,v| !KNOWN_PARAMS.include? k}
     halt 401, 'ERROR_INVALID_HASHKEY' unless auth_hash.valid_request?(filtered_params, params[:hash_key])
   end
 
